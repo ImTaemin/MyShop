c4f61c2590589788151a1d6cd497e9c57fb70611
김태민 <kw3135@gmail.com>
2022-12-07 11:45:31 +0900

    [#B-4] 스웨거 추가, 포트 변경
    - 9090포트 netstat으로 확인이 안됨(재부팅도 해봄). 포트 9000으로 변경

--- api/src/main/java/com/myshop/api/config/WebConfig.java
+++ api/src/main/java/com/myshop/api/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.myshop.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -21,5 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name()
                 );
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
--- api/src/main/java/com/myshop/api/controller/ProviderController.java
+++ api/src/main/java/com/myshop/api/controller/ProviderController.java
@@ -1,10 +1,19 @@
 package com.myshop.api.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/provider")
 public class ProviderController {
+
+    @ApiOperation(value = "판매자 회원가입")
+    @PostMapping("/test")
+    public String test(){
+        return "test";
+    }
 
 }
