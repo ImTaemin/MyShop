4af408fc6478d3d376709ecd080ac928de78c433
김태민 <kw3135@gmail.com>
2023-04-09 05:29:13 +0900

    [#B-37] 함수 어노테이션 버그 수정
    - 진짜 마지막. 이거 안되면 로직 수정

--- api/src/main/java/com/myshop/api/service/KakaoPayServiceImpl.java
+++ api/src/main/java/com/myshop/api/service/KakaoPayServiceImpl.java
@@ -36,16 +36,16 @@ public class KakaoPayServiceImpl implements KakaoPayService{
      * bean간에 주입을 모두 마친 후 @Value 작업 시작하기 때문에 yml 값을 못 읽는 상황이 벌어진다.
      * Environment env를 사용하는 쪽에서 생성자로 보낼 수 있다.
      */
-    @Value("${kakao.url.ready}")
+//    @Value("${kakao.url.ready}")
     private static String readyUrl = "https://kapi.kakao.com/v1/payment/ready";
 
-    @Value("${kakao.url.approve}")
+//    @Value("${kakao.url.approve}")
     private String approveUrl = "https://kapi.kakao.com/v1/payment/approve";
 
-    @Value("${kakao.key.admin}")
+//    @Value("${kakao.key.admin}")
     private String adminKey = "f81ae7787c4fba8226b7798b88502c72";
 
-    @Value("${api.url.baseUrl}")
+//    @Value("${api.url.baseUrl}")
     private String apiBaseUrl = "http://api.myshop.r-e.kr";
 
     private final ItemRepository itemRepository;
